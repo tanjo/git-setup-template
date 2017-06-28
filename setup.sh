12adb811382d0e3f32f8b49a161313cf04a5b0d8
Yuuki Tanjo <tanjo@users.noreply.github.com>
2017-06-28 15:54:58 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "引数は1個だけにしてください"
+  exit 1
+fi
+
+if [ ! -d $1 ]; then
+  echo "ディレクトリが存在しません" 1>&2
+  exit 1
+fi
+
+if [ ! -f $1/.git/config ]; then
+  echo ".git/config が存在しません" 1>&2
+  exit 1
+fi
+
+# githubの設定をする
+pushd $1 > /dev/null 2>&1
+git config user.name "$name$"
+git config user.email "$email$"
+popd > /dev/null 2>&1
+exit 0
